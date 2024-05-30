01e760c2b96be933241f37317fd80d8ea6789e8e
FarhanAmir306 <amirf3051@gmail.com>
2024-05-30 23:38:03 +0600

    problem-solving

---
+++ problem-solve/problem_solving.js
@@ -0,0 +1,81 @@
+// 1. Problem....
+
+let carname = "Volvo" ;
+
+// 2. Problem
+
+let firstName="john";
+let lastName="Doe";
+let age=35;
+
+//3 .problem
+
+let x=10;
+let y=5;
+x=x*y;
+
+//4. problem
+{ 
+
+let length=16; // number 
+let lastname="Johnson"; //string
+
+const x = {
+    firstName: "John",  
+    lastName: "Doe"
+}; //object
+
+}
+
+//5. problem
+function myFunction() {
+ alert("Hello World!");
+}
+ myFunction();
+
+// 6. Problem
+
+const parson={
+    name: "John",
+    age: 50,
+
+}
+alert(parson.name + " is " + parson.age);
+
+
+// 7. Problem
+
+<button onclick="alert('button on going')">Click me.</button>  
+
+// 8. Problem
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+console.log(cars.length);
+
+const Brand = ["Volvo", "Jeep", "Mercedes"];
+Brand[0]='Ford';
+
+// 9. Problem
+
+const random_number = Math.floor(Math.random() * 99) + 1;
+
+const max=Math.max(10,20);
+
+const sqr = Math.sqrt(9);
+
+
+{ 
+// 10. Problem
+
+const x = 10;
+const y = 5;
+
+if (x > y){
+    alert('True')
+}
+else{
+    alert('False')
+}
+
+
+}
